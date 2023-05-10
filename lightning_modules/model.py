11bdec6f2e2e2bb9f46f5b6d7adbbbc4a594efdf
ArtA <pthung7102002@gmail.com>
2023-05-11 03:05:03 +0700

    create compute_loss, organize

--- lightning_modules/model.py
+++ lightning_modules/model.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.optim import Adam
 from torchmetrics import Accuracy
@@ -8,19 +9,19 @@ import lightning.pytorch as pl
 class LitModel(pl.LightningModule):
     """PyTorch Lightning module"""
     
-    def __init__(self, model, lr: float=1e-3):
+    def __init__(self, model: nn.Module, lr: float=1e-3):
         super().__init__()
         self.model = model
         self.learning_rate = lr
         self.accuracy = Accuracy('multiclass', num_classes=10)
 
+    # Feed forward
     def forward(self, x):
         return self.model(x)
 
-    # Log learning rate (remove soon -> move to callback.py)
-    def on_train_epoch_start(self):
-        current_lr = self.optimizers().param_groups[0]['lr']
-        self.log("lr", current_lr)
+    # Compute the loss
+    def compute_loss(self, y_hat, y):
+        return F.cross_entropy(y_hat, y)
 
     # Setup the optimizer
     def configure_optimizers(self):
@@ -37,17 +38,16 @@ class LitModel(pl.LightningModule):
     # Train step
     def training_step(self, batch, batch_idx):
         X, y = batch
-        outputs = self(X)
-        loss = F.cross_entropy(outputs, y)
-        acc = self.accuracy(outputs, y)
+        y_hat = self(X)
+        loss = self.compute_loss(y_hat, y)
+        acc = self.accuracy(y_hat, y)
         self.log_dict({"train_loss": loss, "train_acc": acc})
         return loss
 
     # Validate step
     def validation_step(self, batch, batch_idx):
         X, y = batch
-        outputs = self(X)
-        loss = F.cross_entropy(outputs, y)
-        acc = self.accuracy(outputs, y)
+        y_hat = self(X)
+        loss = self.compute_loss(y_hat, y)
+        acc = self.accuracy(y_hat, y)
         self.log_dict({"val_loss": loss, "val_acc": acc})
-        return loss
